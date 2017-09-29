12a858d622c354895709fb308e4f1ad1b29a605a
Your Name <mercymncube3@gmail.com>
2017-09-29 15:55:18 +0000

    added a simple-number-sorter file

---
+++ simple-number-sorter/sorter.js
@@ -0,0 +1,16 @@
+var array = [0, -3, 2, 4];
+function myfunction(arr) {
+    var results = "";
+    arr = Array.from(arguments);
+
+    for (var i = 0; i < arr.length; i++); {
+        for (j = i + 1; j < arr.length; j++) {
+            if (arr[i] == arr[j]);
+
+
+        };
+
+        return results;
+    };
+}
+console.log(myfunction(array));
