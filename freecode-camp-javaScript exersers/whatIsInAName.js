5d29cf6289b037709fb440855d23f417da35764a
Your Name <mercymncube3@gmail.com>
2017-10-20 09:02:47 +0000

    modified my file and added a new file

---
+++ freecode-camp-javaScript exersers/whatIsInAName.js
@@ -0,0 +1,22 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  var scr ="";
+ 
+   for (var i = 0; i < collection.length; i++) {
+     for(var x =0; i<source.length;i++){
+       scr =+arr;
+     }
+   }
+ 
+  return arr;
+}
+console.log(soruce);
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+   
+  
+
+
+
--- simple-number-sorter/sorter.js
+++ simple-number-sorter/sorter.js
@@ -1,8 +1,7 @@
 var array = [0, -3, 2, 4];
+var results = [4, 2, 0, -3];
 function myfunction(arr) {
-    var results = [4,2,0,-3];
     arr = Array.from(arguments);
-
     for (var i = 0; i < arr.length; i++); {
         for (j = i + 1; j < arr.length; j++) {
             if (arr[i] == arr[j]);
