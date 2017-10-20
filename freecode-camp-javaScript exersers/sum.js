a4937e4ee26e70e46cc32b53eecdf62eb10e05c1
Your Name <mercymncube3@gmail.com>
2017-10-20 09:15:41 +0000

    modified a file

--- freecode-camp-javaScript exersers/sum.js
+++ freecode-camp-javaScript exersers/sum.js
@@ -1,4 +1,4 @@
-function sumAll(arr) {
+     function sumAll(arr) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
     var max = Math.max(arr[0], arr[1]);
     var min = Math.min(arr[0], arr[1]);
     var results= 0;
