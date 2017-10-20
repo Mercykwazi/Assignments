bf835ccfcedf0d14cb0bd86592d1e2b79464f049
Your Name <mercymncube3@gmail.com>
2017-10-20 11:27:42 +0000

    my function is not returning the last element of an array

--- test/test-20-oct-2017/finding-the-last-element.js
+++ test/test-20-oct-2017/finding-the-last-element.js
@@ -1,7 +1,6 @@
-var values =[1,2,3,4];
-function findingLastElement(array){
-   for(var i=0;i<array.length;i++){
-    }
-    return array;
+var values = [1, 2, 3, 4];
+function findingLastElement(arr) {
+ var array = values.pop();
+ return array;
 }
 console.log(findingLastElement(values))
