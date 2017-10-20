6e024fecf843f1bad9663b2f53ce7672d0cbf81e
Your Name <mercymncube3@gmail.com>
2017-10-20 10:46:31 +0000

    my duplication now returns true if there is a duplication in an array

--- test/test-20-oct-2017/duplication.js
+++ test/test-20-oct-2017/duplication.js
@@ -1,2 +1,15 @@
 
-        function arrHasDuplication(interger) {
+
+var data = new Array(1, 3, 6,);
+function arrHasDuplication(interger) {
+    for (i = 0; i < interger.length; i++) {
+        for (j = i + 1; j < interger.length; j++) {
+            if (interger[i] == interger[j]) 
+                return true;
+             }
+            return false;
+
+       
+    }
+}
+console.log(arrHasDuplication(data));
--- test/test-20-oct-2017/finding-the-last-element.js
+++ test/test-20-oct-2017/finding-the-last-element.js
@@ -1,9 +1,6 @@
 var values =[1,2,3,4];
 function findingLastElement(array){
    for(var i=0;i<array.length;i++){
-   while(x < values.length){
-
-   }
     }
     return array;
 }
