d52aba052e69b25de2d763c992a6c61347ddf0e6
Your Name <mercymncube3@gmail.com>
2017-10-20 10:24:33 +0000

    added a new function duplication

--- test/test-20-oct-2017/finding-the-last-element.js
+++ test/test-20-oct-2017/finding-the-last-element.js
@@ -1,3 +1,10 @@
-function findingLastElement(
-    
-)
+var values =[1,2,3,4];
+function findingLastElement(array){
+   for(var i=0;i<array.length;i++){
+   while(x < values.length){
+
+   }
+    }
+    return array;
+}
+console.log(findingLastElement(values))
