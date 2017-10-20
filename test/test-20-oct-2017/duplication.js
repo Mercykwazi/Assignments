3c967bcd1a2da8249bb0c3589c9f50ab71d99d64
Your Name <mercymncube3@gmail.com>
2017-10-20 10:48:34 +0000

    modified my file

--- test/test-20-oct-2017/duplication.js
+++ test/test-20-oct-2017/duplication.js
@@ -1,6 +1,6 @@
 
 
-var data = new Array(1, 3, 6,);
+var data =[1, 3, 6,1];
 function arrHasDuplication(interger) {
     for (i = 0; i < interger.length; i++) {
         for (j = i + 1; j < interger.length; j++) {
