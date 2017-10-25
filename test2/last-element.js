c5a425df6d94314269a65dba05cc256525803d77
Your Name <mercymncube3@gmail.com>
2017-10-25 10:49:16 +0000

    my function now works for all the given conditions

--- test2/last-element.js
+++ test2/last-element.js
@@ -1,22 +1,14 @@
-// 1. Find the last element of a list.
-
-//   Example:
-
-// last( [1,2,3,4] ) // => 4
-
-// last( "xyz" ) // => z
-
-// last( 1,2,3,4 ) // => 4
 
 
-//1. create a function called last (parameters?)
-
-//2. find and return the last element of an array
-
-//3. If it is a string, find the last character in the string and return it
-
-//4. If there are multiple arguments, return the last argument passed in.
-
-function lastElement(){
-    
+function lastElement(last) {
+    var array = Array.from(arguments);
+    if (last.length != undefined && array.length == 1) {
+        return last[last.length - 1]
+    } else {
+        return array[array.length - 1]
+    }
 }
+console.log(lastElement([1,2,3,4]))
+console.log(lastElement(1,2,3,4))
+console.log(lastElement("xyz"))
+
