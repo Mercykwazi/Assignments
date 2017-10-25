43a11ee3bf718089d5dfaa5abb78ea8c20287bd2
Your Name <mercymncube3@gmail.com>
2017-10-25 11:06:23 +0000

    my fuction is now returning the expected

--- test2/prototype.js
+++ test2/prototype.js
@@ -1,7 +1,16 @@
 // 2. Correct this code, so that the greet function returns the expected value.
 
 
-  function Person(name){ this.name = name; }
+function Person(name) { 
+    this.name = name; 
+  }
 
-Person.prototype.greet = function(otherName){ return "Hi " + otherName + ", my name is " + name; }
+  Person.prototype.greet = function (otherName) { 
+      return "Hi " + otherName + ", my name is " + this.name; 
+    }
 
+ var person1 =new Person("Noel");
+ var person2 =new Person("Sandile"); 
+
+ console.log(person1.greet("Mercy"))
+ console.log(person2.greet("Mercy"))
