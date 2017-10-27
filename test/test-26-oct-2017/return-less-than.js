11eeb556afc2756cf3d31534a13cc8fbdbfc54ac
Your Name <mercymncube3@gmail.com>
2017-10-27 11:05:34 +0000

    my function is now returning the expected values

--- test/test-26-oct-2017/return-less-than.js
+++ test/test-26-oct-2017/return-less-than.js
@@ -1,1 +1,15 @@
-function 
+function returnLessThan(array, number) {
+    var arr = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < number) {
+            arr.push(array[i]);
+        }
+    }
+    return arr;
+}
+
+
+
+
+console.log(returnLessThan([1, 2, 3, 4, 10,7], 9))
+console.log("is my console.log working")
