c61ed34c32a9b4f03a4f59e24f69c9c60702101f
Your Name <mercymncube3@gmail.com>
2017-11-09 16:58:13 +0000

    added a statement that showed the  how I got the value of the previous number

--- freecode-camp-javaScript exersers/fibonnacci-numbers.js
+++ freecode-camp-javaScript exersers/fibonnacci-numbers.js
@@ -1,11 +1,12 @@
-function sumFibs(num) { 
-   var prevNumber = 0;
+function sumFibs(num) {
+    var prevNumber = 0;
     var currentNum = 1;
-        var result = 0;
-         while(currentNum <= num) {
-         }
-            if (currentNum % 2 !== 0) {
-            result += currentNum;
-        }
-         currentNum = currentNum+prevNumber; 
+    var result = 0;
+    while (currentNum <= num) {
+    }
+    if (currentNum % 2 !== 0) {
+        result += currentNum;
+    }
+    currentNum = currentNum + prevNumber;
+    prevNumber = currentNum - prevNumber;
 }
