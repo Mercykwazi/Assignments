87ab16ef5e303c47e505f9aba4ca10040c3d3ad4
Your Name <mercymncube3@gmail.com>
2017-11-10 12:09:11 +0000

    my function is now working

--- test/test-10-Nov-2017/multiplier.js
+++ test/test-10-Nov-2017/multiplier.js
@@ -1,20 +1,9 @@
-/*Write a multiply method which will perform correctly when called using either syntax below.
 
-console.log(multiply(3,3));   //Output 9
-console.log(multiply(3)(3));  // Outputs 9*/
-function addition(a,b) {
-    return a *b;
-}
-function addition2(val,val2){
+function multiply(val) {
+function multiple(val2){
     return val * val2;
 }
-function multiply(addition,addition2){
-    return addition  * addition2;
+return multiple;
 }
-  /*  var array =Array.from(arguments);
-    var myVar = array.reduce(function (value,number) {
-        return value * number;
-    },)
-    return myVar;
-}*/
 console.log(multiply(3)(3));
+
