eb467e351d977e07223d2bedec9a6bf328fa08f6
Your Name <mercymncube3@gmail.com>
2017-11-14 12:48:47 +0000

    removed all commented code

--- freecode-camp-javaScript exersers/sum-of-prime-numbers.js
+++ freecode-camp-javaScript exersers/sum-of-prime-numbers.js
@@ -6,11 +6,6 @@ function isPrime(number) {
     }
     return true;
 }
-/*Sum all the prime numbers up to and including the provided number.
-
-A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
-
-*/
 function sumPrime(num) {
     var array = [];
     for (var i = 2; i <= num; i++) {
