27c8486e634dc9f6895e98729b5e92465287f1f7
Your Name <mercymncube3@gmail.com>
2017-11-14 12:35:25 +0000

    added curly braces to my function but it is not returning the expected value

--- freecode-camp-javaScript exersers/sum-of-prime-numbers.js
+++ freecode-camp-javaScript exersers/sum-of-prime-numbers.js
@@ -12,12 +12,13 @@ console.log(isPrime(25));
 A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
 
 */
- function sumPrime(num)
-  //var array = [];
+function sumPrime(num) {
+    var array = [];
     for (var i = 2; i <= num; i++) {
-    if (isPrime(i)) {
-        array.push(i);
+        if (isPrime(i)) {
+            array.push(i);
+        }
+        return array;
     }
-    return array;
 }
 console.log(sumPrime(10))
