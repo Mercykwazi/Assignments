c39a3b3823fb2f97525abb2905d8fe86306f26b9
Your Name <mercymncube3@gmail.com>
2017-11-14 12:31:58 +0000

    added the secound function but its throwing  an errow

--- freecode-camp-javaScript exersers/sum-of-prime-numbers.js
+++ freecode-camp-javaScript exersers/sum-of-prime-numbers.js
@@ -1,9 +1,23 @@
-function isPrime(number){
-    for(var i =2;i<number;i++){
-        if(number % i ===0){
+function isPrime(number) {
+    for (var i = 2; i < number; i++) {
+        if (number % i === 0) {
             return false;
         }
     }
     return true;
 }
 console.log(isPrime(25));
+/*Sum all the prime numbers up to and including the provided number.
+
+A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+
+*/
+ function sumPrime(num)
+  //var array = [];
+    for (var i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+        array.push(i);
+    }
+    return array;
+}
+console.log(sumPrime(10))
