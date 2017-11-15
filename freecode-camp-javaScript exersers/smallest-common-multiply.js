f1cc67f570804687b58377ee50bdb87b574f5216
Your Name <mercymncube3@gmail.com>
2017-11-15 13:51:26 +0000

    sorted my array and added variable

--- freecode-camp-javaScript exersers/smallest-common-multiply.js
+++ freecode-camp-javaScript exersers/smallest-common-multiply.js
@@ -1,4 +1,13 @@
-function commonMultiple(array){
-return array;
+function commonMultiple(array) {
+    var sorted = array.sort();
+    var arr = [];
+    var first = array[0];
+    var second = array[1];
+    return array[1];
 }
-console.log(commonMultiple([1,5]))
+console.log(commonMultiple([1, 5]))
+/*Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+
+The range will be an array of two numbers that will not necessarily be in numerical order.
+
+e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3 that is evenly divisible by all numbers between 1 and 3.*/
