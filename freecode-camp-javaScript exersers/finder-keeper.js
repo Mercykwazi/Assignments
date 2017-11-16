3d8977066ec07826e0fcb365b93fbaff9773b14d
Your Name <mercymncube3@gmail.com>
2017-11-16 11:34:38 +0000

    added validation  function

--- freecode-camp-javaScript exersers/finder-keeper.js
+++ freecode-camp-javaScript exersers/finder-keeper.js
@@ -1,4 +1,9 @@
 function findElement(arr, func) {
+    function validate(num) {
+        if (num % 2 === 0) {
+            return num;
+        };
+    };
 return  arr && func();
 }
 console.log(findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0; }))
