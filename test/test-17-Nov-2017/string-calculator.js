435ce07db220d2780c589631c7c9ee3947ad4122
Your Name <mercymncube3@gmail.com>
2017-11-17 12:17:07 +0000

    added a reduce function but my function is not yet working

--- test/test-17-Nov-2017/string-calculator.js
+++ test/test-17-Nov-2017/string-calculator.js
@@ -4,11 +4,23 @@
     Start with the simplest output of an empty string and move to 1 and two numbers
     Remember to solve things as simply as possible so that you force yourself to write test scenarios you did not think about
     Remember to refactor after each successful output Allow the Add method to handle an unknown amount of numbers*/
-function addString() {
+function addString(string) {
+    var arr = [];
     var array = Array.from(arguments);
-    return array;
+    for (var i = 0; i < array.length; i++) {
+        arr.push(array[i])
+    }
+    var changed = Number(arr);
+    var sum = arr.reduce(function (a, b) {
+        return a + b;
+    });
 
+
+    return sum;
 }
 console.log(addString("1", "2"))
+console.log(addString("1", "2", "7"))
+console.log(addString("1"))
+console.log(addString(""))
 
 
