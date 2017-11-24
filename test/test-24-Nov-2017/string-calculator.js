841478504589710fccf3b0e5dc716c766ffb87bd
Your Name <mercymncube3@gmail.com>
2017-11-24 10:11:46 +0000

    added an if-statement by my function is returning undifined

--- test/test-24-Nov-2017/string-calculator.js
+++ test/test-24-Nov-2017/string-calculator.js
@@ -3,5 +3,9 @@ function addInt(string) {
     var arr = [];
     for (var i = 0; i < spited.length; i++) {
         arr.push(spited[i])
+           if (arr[i] == "") {
+            return 0;
+        };
     }
 }
+console.log(addInt("1,2"))
