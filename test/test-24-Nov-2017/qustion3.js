8f543fb820c5d58e506d60f58ba47a0c5fe74aea
Your Name <mercymncube3@gmail.com>
2017-11-24 13:32:20 +0000

    added a new function that replace the new line

--- test/test-24-Nov-2017/qustion3.js
+++ test/test-24-Nov-2017/qustion3.js
@@ -1,6 +1,8 @@
 function addInt(string) {
    var newLine = string.replace(/\n/g, ",");
    var spited = newLine.split(',');
+   var matched = spited.match(/d/g)
+
 
     for (var i = 0; i < spited.length; i++) {
         if (spited[i] == "") {
@@ -12,7 +14,7 @@ function addInt(string) {
         return parseInt(a) + parseInt(b);
     });
 
-    return sum;
+    return matched;
 }
 console.log(addInt("1,2"))
 console.log(addInt(""))
