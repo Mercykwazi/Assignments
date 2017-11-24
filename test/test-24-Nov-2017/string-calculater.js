a99cb9b6719f2493ac510ae3921237a2cb3dc22d
Your Name <mercymncube3@gmail.com>
2017-11-24 16:02:54 +0000

    my function now throws an error

--- test/test-24-Nov-2017/string-calculater.js
+++ test/test-24-Nov-2017/string-calculater.js
@@ -2,19 +2,23 @@ function addInt(string) {
     if (string == "") {
         return 0;
     } else {
-        var numbers = string.match(/[0-9]/g);
+        var numbers = string.match(/(-[0-9])|([0-9])/g);
+        for (var i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+               return   Error("negatives not allowed")
+            }
+        }
         var sum = numbers.reduce(function (a, b) {
             return parseInt(a) + parseInt(b);
         });
         return sum;
     }
-
 }
 
 
 
 console.log(addInt("1,2"))
-console.log(addInt("-1,2"))
+console.log(addInt("-1,-2"))
 console.log(addInt(""))
 console.log(addInt("1\n2,3"));
 console.log(addInt("1//;n2,3"));
