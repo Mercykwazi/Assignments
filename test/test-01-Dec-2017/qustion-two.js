c1ed11d14d083e53c4181b4a89f661df020c9705
Your Name <mercymncube3@gmail.com>
2017-12-01 10:50:12 +0000

    my function is now working

--- test/test-01-Dec-2017/qustion-two.js
+++ test/test-01-Dec-2017/qustion-two.js
@@ -1,4 +1,5 @@
-/*Write a function that will return "And" if both the Conditions are met, "Or" if only one of the conditions are met or "None" if neither of the conditions are met. The function should take one argument and should check if it is an integer and if it is greater than 0. Name the function yourself.
+/*Write a function that will return "And" if both the Conditions are met, "Or" if only one of the conditions are met or "None" if neither of the conditions are met.
+ The function should take one argument and should check if it is an integer and if it is greater than 0. Name the function yourself.
 
 
 ExampleFunction(1) = "And"
@@ -6,3 +7,17 @@ ExampleFunction(1) = "And"
 ExampleFunction(-1) = "Or"
 
 ExampleFunction("a") = "None"*/
+
+function whatIsIt(integer) {
+    var results = "";
+    if (integer > 0) {
+        results = "And";
+    } else if (integer < 0) {
+        results = "Or"
+    } else {
+        results = "None"
+    }
+ 
+    return results;
+}
+console.log(whatIsIt("a"))
