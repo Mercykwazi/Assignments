33de8996f69df7ee1955d95508cd1c217dc9d4f2
Your Name <mercymncube3@gmail.com>
2017-12-01 11:57:16 +0000

    modified my file

--- test/test-01-Dec-2017/boolean.js
+++ test/test-01-Dec-2017/boolean.js
@@ -1,20 +1,13 @@
-/*Create a function that will return true if the second argument (string) is in the first argument (string). Please create your own name for the function, do not use the example.
-someFunctionName('Perfect Practice Makes Perfect', 'Perfect') = True
-someFunctionName('We should have a growth Mindset', 'Fixed') = False
-*/
+
 function isIt(str, str2) {
-    for (var i = 0; i < str.length; i++) {
-        for (var x = 0; x < str2.length; x++) {
-            if (str[i] === str2[x]) {
-                return true;
-            }
-        }
-        return false;
+    if(str.indexOf(str2)!==-1){
+        return true
     }
+        return false;
 
 }
 console.log(isIt("We should have a growth Mindset", "Fixed"))//false
 console.log(isIt("Perfect Practice Makes Perfect", "Perfect"))//true
 console.log(isIt("My name is Mercy","Mercy"))//true
-console.log(isIt("This is the first flight to cape","that"))//true
+console.log(isIt("This is the first flight to cape","is"))//true
 
