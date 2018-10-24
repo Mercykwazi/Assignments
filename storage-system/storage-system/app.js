3a7db19988efed5e614065b730d2d76d8153c064
mercykwazi <you@example.com>
2018-10-24 13:35:28 +0200

    added a post method that gets the business and its location

--- storage-system/storage-system/app.js
+++ storage-system/storage-system/app.js
@@ -34,9 +34,9 @@ app.post('/business', async (req, res) => {
 
 
 
-app.get('/business', async(req, res) => {
+app.get('/business', async (req, res) => {
   try {
-    var businessDetails =  await client.query('SELECT id,business_name  FROM business', (err, result) => {
+    var businessDetails = await client.query('SELECT id,business_name  FROM business', (err, result) => {
       res.send(result)
       res.status(2001).end()
     })
@@ -45,51 +45,34 @@ app.get('/business', async(req, res) => {
     res.status(500).end();
   }
 })
-app.post('/location', async(req, res) => {
-  const insertLocations = 'INSERT INTO location(address1,address2,country)VALUES($1,$2,$3)';
-
-  const locationDetails = [req.body.address1, req.body.address2, req.body.country]
-  console.log("loc",locationDetails);
+app.post('/location', async (req, res) => {
+  const businessId = await client.query('SELECT id FROM business WHERE business_name=$1', [req.body.businessName]);
+  const insertLocations = 'INSERT INTO location(address1,address2,country,business_id)VALUES($1,$2,$3,$4)';
+  const locationDetails = [req.body.address1, req.body.address2, req.body.country, businessId.rows[0].id]
   try {
     const Results = await client.query(insertLocations, locationDetails)
-    res.status(302).end()
-    console.log('res',Results);
-    
+    res.status(201).end()
+    console.log('res', Results);
+
   } catch (err) {
     console.log(err);
     res.status(500).end()
   }
 })
 
+// app.get('/location', async (req, res) => {
+//   try {
+//     console.log(req.body);
 
-// async function getAllBusinessWithLocations() {
-//   const businessNames = await client.query(`SELECT name FROM business INNER JOIN locations on business.id = locations.business_id;`);
-//   return businessNames.rows;
-// };
-// async function insertBusinessLocation(businessName, country, address1, address2, address3) {
-//   const businessId = await client.query(`SELECT id FROM business WHERE name = $1;`, [businessName]);
-//   await client.query("INSERT INTO locations(country, address1,address2, address3, business_id) VALUES ( $1,$2,$3,$4,$5 )", [country ? country : null, address1 ? address1 : null, address2 ? address2 : null, address3 ? address3 : null, businessId.rows[0].id > 0 ? businessId.rows[0].id : null]);
-// };
-// async function insertBusinessInfo(businessName, contactName, telephone, email) {
-//   var addingBusiness = await client.query("INSERT INTO business(name, contact_name, contact_number, contact_email) VALUES ($1,$2,$3,$4)", [businessName ? businessName : null, contactName ? contactName : null, telephone ? telephone : null, email ? email : null]);
-//   return addingBusiness;}
+//     var allLocations = await client.query("SELECT address1,address2,country FROM location INNER JOIN business  ON location.business_id = business.id WHERE business_name = $1;", [req.body.businessName])
+//     console.log(allLocations);
 
+//     console.log('res', result);
+//   } catch (error) {
+//     console.log(error);
 
-
-
-
-app.get('/location', (req, res) => {
-  try {
-
-    var allLocations = client.query('SELECT location FROM business INNER JOIN location  ON business.id=location.business_id', (err, result) => {
-      res.send(result)
-      console.log('res', result);
-    })
-  } catch (error) {
-    console.log(error);
-
-  }
-})
+//   }
+// })
 app.post('/unitType', (req, res) => {
   var unitType = {
     storageType: req.body.storageType,
