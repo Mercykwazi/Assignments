5462d048a1071793c551d36f8b4a4383b0d22314
mercykwazi <you@example.com>
2018-10-30 16:01:01 +0200

    removed redux forms and used normal html form

--- storage-system/storage-system/src/components/forms/unit-type.js
+++ storage-system/storage-system/src/components/forms/unit-type.js
@@ -1,44 +1,94 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import * as actions from '../../actions/location';
+import * as actions from '../../actions/unit-type';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import { log } from 'util';
-import { Field, reduxForm } from 'redux-form';
-import { Redirect } from 'react-router'
+
+
+
 class UnitType extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            unitType: this.props.unitType,
+            length: this.props.length,
+            width: this.props.width,
+            height: this.props.height,
+            businessName: this.props.business,
         }
+        this.locationName = this.locationName.bind(this)
+        this.length = this.length.bind(this)
+        this.width = this.width.bind(this)
+        this.height = this.height.bind(this)
+        this.submit = this.submit.bind(this)
     }
 
+    length(e) {
+        var change = {};
+        change[e.target.name] = e.target.value;
+        this.props.updateLength(e.target.value)
+        this.setState(change);
+    }
+
+    locationName(e) {
+        var change = {};
+        change[e.target.name] = e.target.value;
+        this.props.updateName(e.target.value)
+        this.setState(change);
+    }
+    width(e) {
+        var change = {};
+        change[e.target.name] = e.target.value;
+        this.props.updateWidth(e.target.value)
+        this.setState(change);
+    }
+    height(e) {
+        var change = {};
+        change[e.target.name] = e.target.value;
+        this.props.updateHeight(e.target.value)
+        this.setState(change);
+    }
+
+
+    async submit(e) {
+        e.preventDefault()
+        var insertUnits = {
+            storageType: this.props.unitType,
+            length: this.props.length,
+            width: this.props.width,
+            height: this.props.height,
+            selectedBusiness: this.state.businessName
+        }
+        var unitTypeDetails = await axios.post("http://localhost:3003/unitType", insertUnits)
+
+    }
     render() {
-        const { handleSubmit } = this.props
         return (<div>
-            <h1>Your Location Details Below</h1>
-            <form >
-                <div>
-                    <label htmlFor="firstName">Storage type:</label><br />
-                    <Field name="firstName" component="input" type="text" />
+            <h1>Specify your unit type</h1>
+            <form>
+                <div className="business">
+                    <div >
+                        <label htmlFor="unitType">Unit type:</label><br />
+                        <input name="unitType" placeholder="garage/warehouse" type="text" onChange={this.locationName} value={this.state.unitType} />
+                    </div>
+                    <br />
+                    <div >
+                        <label htmlFor="length">Length(m):</label><br />
+                        <input name="length" placeholder='1200' type="number" onChange={this.length} value={this.state.length} />
+                    </div>
+                    <br />
+                    <div >
+                        <label htmlFor="width">Width(m):</label><br />
+                        <input name="width" placeholder='1200' type="number" onChange={this.width} value={this.state.width} />
+                    </div>
+                    <br />
+                    <div >
+                        <label htmlFor="height">height(m):</label><br />
+                        <input name="height" placeholder='800' type="number" onChange={this.height} value={this.state.height} />
+                    </div>
+                    <br />
+                    <button onClick={this.submit}>submit</button>
                 </div>
-                <div>
-                    <label htmlFor="lastName">Length</label><br />
-                    <Field name="lastName" component="input" type="number" />
-                </div>
-                <div>
-                    <label htmlFor="email">width</label><br />
-                    <Field name="email" component="input" type="number" />
-                </div>
-                <div>
-                    <label htmlFor="email">Hight</label><br />
-                    <Field name="email" component="input" type="number" />
-                </div>
-                <div>
-                    <label htmlFor="email">Storage name</label><br />
-                    <Field name="email" component="input" type="number" />
-                </div>
-                <button onClick={handleSubmit} type="submit">Submit</button>
             </form>
 
         </div>
@@ -46,9 +96,30 @@ class UnitType extends React.Component {
     }
 
 }
-UnitType = reduxForm({
-    form: 'contact'
-})(UnitType)
+const mapStateToProps = (state) => {
+    return {
+        unitType: state.unitType.unitName,
+        length: state.unitType.length,
+        width: state.unitType.width,
+        height: state.unitType.height,
+        business: state.viewBusiness.selectedBusiness,
+        state: state
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateName: name => {
+            dispatch(actions.unitType(name))
+        },
+        updateLength: length => {
+            dispatch(actions.length(length))
+        },
+        updateWidth: width => {
+            dispatch(actions.width(width))
+        }, updateHeight: height => {
+            dispatch(actions.height(height))
+        }
+    }
+}
 
-export default UnitType
-
+export default connect(mapStateToProps, mapDispatchToProps)(UnitType)
