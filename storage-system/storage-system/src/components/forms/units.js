6d7a40bc72032b5f21703aff30b3bd90eb792fb4
mercykwazi <you@example.com>
2018-11-06 11:57:15 +0200

    removed the code in the next function to the submit function. added clss name for the next button .

--- storage-system/storage-system/src/components/forms/units.js
+++ storage-system/storage-system/src/components/forms/units.js
@@ -36,56 +36,34 @@ class Units extends React.Component {
     async unitTypeDetails() {
         var results = await axios.get("http://localhost:3003/unitType/")
         var unitType = results.data.rows
-
         this.setState({ unitTypeDetail: unitType })
-
     }
 
     async unitsDetails(e) {
-        console.log('what is this');
-
         e.preventDefault();
+        var findTheItem = this.props.unitType.split(" ")
+        var unitType = this.state.unitTypeDetail
+        var results = unitType.find(item => {
+            var returningObjects = item.name === findTheItem[0] && item.length === findTheItem[1] && item.width === findTheItem[2] && item.height === findTheItem[3]
+            return returningObjects
+        })
         var units = {
             name: this.props.units,
             id: this.state.unitTypeDetail,
             selectedUnit: this.props.unitType,
             selectedBusiness: this.props.business,
+            foundObject: results
         }
-
-        console.log('yes am called', units);
-        // var results = await axios.post("http://localhost:3003/units/", units)
-
+        var results = await axios.post("http://localhost:3003/units/", units)
     }
     next(e) {
         e.preventDefault()
-        console.log("findTheItem1", this.props.unitType);
-        
-        var findTheItem = this.props.unitType.split(" ")
-        console.log("findTheItem", findTheItem);
-        var unitType = this.state.unitTypeDetail
-        var results = unitType.filter(item => {
-            var returningObjects = item.name === findTheItem[0]
-            console.log('what the hell11', returningObjects);
-
-            return returningObjects
-        }).map(item=>{
-            console.log('wat is this',item);
-            
-        })
-
-        console.log('what the hell', results);
-
     }
 
-    // next() {
-    //     this.setState({ redirect: true })
-    // }
-
     render() {
-        console.log('props', this.props.unitType, this.state.unitTypeDetail);
-
         return (<div>
             <div className="selectedUnit">
+                <p>Available unit types</p>
                 <select onChange={(e) => this.props.selectedUnitType(e.target.value)}>
                     <option value="Select unit type">Select Unit type:</option>
                     {this.state.unitTypeDetail.length > 0 ? this.state.unitTypeDetail.map(item => {
@@ -93,16 +71,16 @@ class Units extends React.Component {
                     }) : null}
                 </select>
             </div>
-            <h1>units available</h1>
+
             <form >
-                <div className="location">
+                <div className="units">
                     <div >
                         <label htmlFor="name">Unit Name:</label><br />
-                        <input name="name" type="text" onChange={this.unitName} value={this.state.name} />
+                        <input name="name" type="text" onChange={this.unitName} value={this.state.name} /> <button className="button" onClick={this.unitsDetails}>add</button>
                     </div>
                     <br />
-                    <div><button onClick={this.unitsDetails}>add</button></div><br />
-                    <button onClick={this.next}>next</button>
+                    <div></div><br />
+                    <button className="next" onClick={this.next}>next</button>
 
                 </div>
             </form>
