baf323120d0c4e99149c5404204db9d4c755f4d5
mercykwazi <you@example.com>
2019-03-08 08:26:39 +0200

    aded the service and depolyment file

---
+++ storage-system/storage-system/react/build/precache-manifest.e5cdd6a491e418910ffadce51ebf5f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "619f693ddc5c39d8b9f937d2c9625c58",
+    "url": "/static/media/just.619f693d.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9a0c9e625cc3728484d5",
+    "url": "/static/js/main.9a0c9e62.chunk.js"
+  },
+  {
+    "revision": "e84ee357d82bdbaaffb1",
+    "url": "/static/js/2.e84ee357.chunk.js"
+  },
+  {
+    "revision": "9a0c9e625cc3728484d5",
+    "url": "/static/css/main.d0a7bfb6.chunk.css"
+  },
+  {
+    "revision": "9669dca0af58f7cc2cf877d39447a87c",
+    "url": "/index.html"
+  }
+];
