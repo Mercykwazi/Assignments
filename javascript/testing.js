b8b751faf0050ba8e8d1352ac2f62cfa811316b6
mercykwazi <you@example.com>
2019-05-16 15:16:00 +0200

    added a new file xhub

---
+++ javascript/testing.js
@@ -0,0 +1,14 @@
+// function sayMyname(name) {
+//   return name;
+// }
+// function myName(name) {
+//   console.log("My name is", name);
+// }
+function add(value1, value2) {
+  var results = value1 + value2;
+  console.log("results are", results);
+
+  return results;
+  console.log("will I be called?", results);
+}
+add(9, 7);
